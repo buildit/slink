1f963598781bfa23ab40ec426e12988dacb96738
Chih-Ho Andy Chou <chihho.chou@wipro.com>
2018-05-29 12:14:22 -0400

    - Resolved issue #18; used jest mock funstions got automatic mock and removed manual mock and __mocks__ directory

--- slink-main/tests/unit/handler_test.js
+++ slink-main/tests/unit/handler_test.js
@@ -9,15 +9,20 @@ let event;
 let context;
 
 describe('Tests index', () => {
-  it('verifies mock successful response', async () => {
-    smartrecruiters.setWillReturnSuccessResult(true);
+  beforeEach(() => {
+    // Clear all instances and calls to constructor and all methods
+    smartrecruiters.getCandidateSummaries.mockClear();
+  });
+  it('verifies successful response', async () => {
+    const mockSuccessResult = { message: 'Candidates found', count: 1 };
+    smartrecruiters.getCandidateSummaries.mockResolvedValue(mockSuccessResult);
     await index.handler(event, context, (err, result) => {
       expect(result.statusCode).toEqual(200);
       expect(getType(result.body)).toEqual('string');
     });
   });
-  it('verifies mock failed response', async () => {
-    smartrecruiters.setWillReturnSuccessResult(false);
+  it('verifies failed response', async () => {
+    smartrecruiters.getCandidateSummaries.mockRejectedValue(new Error('Error'));
     await index.handler(event, context, (err, result) => {
       expect(result.statusCode).toEqual(500);
       expect(getType(result.body)).toEqual('object');
--- slink-main/tests/unit/smartrecruiters_test.js
+++ slink-main/tests/unit/smartrecruiters_test.js
@@ -7,10 +7,13 @@ jest.mock('axios');
 
 
 describe('Tests get candidate summary', () => {
-  beforeEach(() => {
-    jest.resetModules();
+  beforeAll(() => {
     process.env.SR_API_TOKEN = 'SR_API_TOKEN';
     process.env.SR_CANDIDATE_SUMMARY_URL = 'http://mockurl/';
+  });
+
+  beforeEach(() => {
+    axios.mockClear();
   });
 
   it('should get user summary when sucess', async () => {
@@ -19,7 +22,7 @@ describe('Tests get candidate summary', () => {
     const result = await smartrecruiters.getCandidateSummaries();
     expect(result.count).toBeGreaterThan(0);
   });
-  it('should return an error when faikl', async () => {
+  it('should return an error when fail', async () => {
     axios.mockRejectedValue(new Error('There is an error'));
     const result = await smartrecruiters.getCandidateSummaries();
     const isErrorInstance = result instanceof Error;
