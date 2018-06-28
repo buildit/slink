cfd7d1dbc87717884fcb7e49a7b019ed5793a1db
Mike Thomas <mathomas@users.noreply.github.com>
2018-06-28 16:08:18 -0600

    Very simple code to write to DynamoDb table ('foo': created in console).

--- slink-main/index.js
+++ slink-main/index.js
@@ -2,9 +2,25 @@
 
 const introduction = require('./introduction');
 const config = require('./config');
+const aws = require('aws-sdk');
+
+async function write(requestId) {
+  const dynamodb = new aws.DynamoDB();
+
+  const params = {
+    Item: {
+      requestId: {
+        S: requestId
+      }
+    },
+    TableName: process.env.INTRODUCTION_EXECUTION_TABLE
+  };
+  await dynamodb.putItem(params).promise();
+}
 
 module.exports.handler = async (event, context, callback) => {
   try {
+    console.log(`#### context:  ${JSON.stringify(context)}`);
     console.log(`#### Function ARN:  ${context.invokedFunctionArn}`);
 
     // Load all configuration parameters from AWS SSM
@@ -16,6 +32,14 @@ module.exports.handler = async (event, context, callback) => {
     result.applicantsIntroducedToSap.forEach((applicant) => {
       console.log(`${JSON.stringify(applicant)}`);
     });
+
+    console.log(`Writing invocationId to Dynamo: ${context.awsRequestId}`);
+    try {
+      await write(context.awsRequestId);
+    } catch (e) {
+      console.log('Write to Dynamo unsuccessful', e);
+    }
+    console.log('Write to Dynamo successful');
 
     const response = {
       statusCode: 200,
